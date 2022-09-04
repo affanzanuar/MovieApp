2e708022fb10038129582c7f71111fcdf2e44892
Krisna Sirait <krisnapande00@outlook.com>
2022-09-04 21:53:59 +0700

    added series adapter and the function

---
+++ app/src/main/java/com/affan/movieapp/view/main/Series/SeriesAdapter.kt
@@ -0,0 +1,42 @@
+package com.affan.movieapp.view.main.Series
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.affan.movieapp.databinding.CardLayoutBinding
+
+class SeriesAdapter : RecyclerView.Adapter<SeriesAdapter.SeriesViewHolder>() {
+
+    private val series = arrayListOf<SeriesData>()
+
+    inner class SeriesViewHolder(
+        private val binding: CardLayoutBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(seriesData: SeriesData) {
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SeriesViewHolder {
+        return SeriesViewHolder(
+            CardLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: SeriesViewHolder, position: Int) {
+        holder.bind(series[position])
+    }
+
+    override fun getItemCount(): Int {
+        return series.size
+    }
+
+    fun setData(data: List<SeriesData>) {
+        series.clear()
+        series.addAll(data)
+        notifyDataSetChanged()
+    }
+
+}
+
