37a0d98d80627843bf1f22c89a824f4d25032774
Krisna Sirait <krisnapande00@outlook.com>
2022-09-07 16:09:16 +0700

    change package name for series package

--- app/src/main/java/com/affan/movieapp/view/main/series/SeriesFragment.kt
+++ app/src/main/java/com/affan/movieapp/view/main/series/SeriesFragment.kt
@@ -1,17 +1,12 @@
-package com.affan.movieapp.view.main.Series
+package com.affan.movieapp.view.main.series
 
-import android.content.ClipData
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.affan.movieapp.R
 import com.affan.movieapp.databinding.FragmentSeriesBinding
-import kotlinx.android.synthetic.main.fragment_series.*
 
 class SeriesFragment : Fragment() {
 
