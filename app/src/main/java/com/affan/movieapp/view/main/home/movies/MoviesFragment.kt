a82782e347e6bfb98406ccb2db849eebe92de899
Krisna Sirait <krisnapande00@outlook.com>
2022-09-07 16:16:21 +0700

    file rename for fragment_movies.xml and MoviesFragment.kt

--- app/src/main/java/com/affan/movieapp/view/main/home/movies/MoviesFragment.kt
+++ app/src/main/java/com/affan/movieapp/view/main/home/movies/MoviesFragment.kt
@@ -35,7 +35,7 @@ class MovieFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie, container, false)
+        return inflater.inflate(R.layout.fragment_movies, container, false)
     }
 
     companion object {
