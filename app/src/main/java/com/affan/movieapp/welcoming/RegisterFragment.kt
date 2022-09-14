e094f108dad6d1f9725a73d6acc1f7e918dd098b
Krisna Sirait <krisnapande00@outlook.com>
2022-09-14 19:09:29 +0700

    implement view binding for LoginFragment.kt and RegisterFragment.kt

--- app/src/main/java/com/affan/movieapp/welcoming/RegisterFragment.kt
+++ app/src/main/java/com/affan/movieapp/welcoming/RegisterFragment.kt
@@ -6,13 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.affan.movieapp.R
+import com.affan.movieapp.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
+
+    private lateinit var binding: FragmentRegisterBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 }
