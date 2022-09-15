690ebdde22b7d1091d233c100010d90bd3807f87
Krisna Sirait <krisnapande00@outlook.com>
2022-09-15 19:18:49 +0700

    changes from fragment into an activity for the login and register

--- app/src/main/java/com/affan/movieapp/welcoming/LoginActivity.kt
+++ app/src/main/java/com/affan/movieapp/welcoming/LoginActivity.kt
@@ -4,11 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.affan.movieapp.R
 
-class CredentialsActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_credentials)
-
-
+        setContentView(R.layout.activity_login)
     }
 }
