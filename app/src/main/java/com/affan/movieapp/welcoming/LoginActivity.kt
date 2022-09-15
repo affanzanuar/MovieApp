571b1fca7a63822e77fafb3ca25cff8d79d2f5ba
Krisna Sirait <krisnapande00@outlook.com>
2022-09-15 19:24:58 +0700

    setonclick listener for btn login and register text

--- app/src/main/java/com/affan/movieapp/welcoming/LoginActivity.kt
+++ app/src/main/java/com/affan/movieapp/welcoming/LoginActivity.kt
@@ -1,12 +1,28 @@
 package com.affan.movieapp.welcoming
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.affan.movieapp.R
+import com.affan.movieapp.databinding.ActivityLoginBinding
+import com.affan.movieapp.view.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.tvRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
