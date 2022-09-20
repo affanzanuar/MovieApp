8cd2ce6f4a21960d7318ba593671e890983806fe
Krisna Sirait <krisnapande00@outlook.com>
2022-09-20 20:16:59 +0700

    added intent do details in SeriesFragment.kt

--- app/src/main/java/com/affan/movieapp/view/main/series/SeriesFragment.kt
+++ app/src/main/java/com/affan/movieapp/view/main/series/SeriesFragment.kt
@@ -1,5 +1,6 @@
 package com.affan.movieapp.view.main.series
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,9 @@ import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.affan.movieapp.databinding.FragmentSeriesBinding
+import com.affan.movieapp.model.movie.Movie
+import com.affan.movieapp.view.main.details.DetailsActivity
+import com.affan.movieapp.view.main.home.HomeFragment
 import com.affan.movieapp.view.main.series.adapter.SeriesAdapter
 import com.affan.movieapp.view.main.series.presenter.SeriesPresenter
 import com.affan.movieapp.view.main.series.presenter.SeriesView
@@ -39,6 +43,27 @@ class SeriesFragment : Fragment(), SeriesView {
         seriesAdapter = SeriesAdapter()
         binding.rvSeries.adapter = seriesAdapter
         binding.rvSeries.layoutManager = GridLayoutManager(context, 2)
+    }
+
+    private fun intentToDetails(series: Series) {
+        val intent = Intent(context, DetailsActivity::class.java)
+        val parcelable = Series (
+            series.backdropPath,
+            series.firstAirDate,
+            series.genreIds,
+            series.id,
+            series.name,
+            series.originCountry,
+            series.originalLanguage,
+            series.originalName,
+            series.overview,
+            series.popularity,
+            series.posterPath,
+            series.voteAverage,
+            series.voteCount
+        )
+        intent.putExtra(HomeFragment.EXTRA_DATA_MS,parcelable)
+        startActivity(intent)
     }
 
     private fun createPresenter() {
