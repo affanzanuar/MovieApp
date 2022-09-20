f117fcd27f91f287045a685d3a3b72ee967477f8
Affan Zanuar <affanzanuar1@gmail.com>
2022-09-20 15:01:10 +0700

    "Add Coming Soon model"

---
+++ app/src/main/java/com/affan/movieapp/model/comingsoon/ComingSoonResponse.kt
@@ -0,0 +1,15 @@
+package com.affan.movieapp.model.comingsoon
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ComingSoonResponse(
+    @SerializedName("page")
+    val page: Int?,
+    @SerializedName("results")
+    val results: List<ComingSoon?>?,
+    @SerializedName("total_pages")
+    val totalPages: Int?,
+    @SerializedName("total_results")
+    val totalResults: Int?
+)
