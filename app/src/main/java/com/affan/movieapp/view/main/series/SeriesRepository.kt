33393aaa6dae2d0b14e67716fd2df40a110afe51
Krisna Sirait <krisnapande00@outlook.com>
2022-09-23 16:54:31 +0700

    added function to return getMostPopularSeries in SeriesRepository.kt

--- app/src/main/java/com/affan/movieapp/view/main/series/SeriesRepository.kt
+++ app/src/main/java/com/affan/movieapp/view/main/series/SeriesRepository.kt
@@ -1,4 +1,13 @@
 package com.affan.movieapp.view.main.series
 
+import com.affan.movieapp.data.Data
+import com.affan.movieapp.model.series.SeriesResponse
+import com.affan.movieapp.network.ApiClient
+import retrofit2.Call
+import retrofit2.Response
+
 class SeriesRepository {
+    suspend fun getPopularSeries(): Call<SeriesResponse> {
+        return ApiClient.instance.getMostPopularSeries2(Data.apiKey)
+    }
 }
