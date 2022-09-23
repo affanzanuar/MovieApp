8c07c561252b898d5863fa3f096a03d800065447
Krisna Sirait <krisnapande00@outlook.com>
2022-09-23 17:08:17 +0700

    migrate from presenter to viewmodel in SeriesFragment.kt

--- app/src/main/java/com/affan/movieapp/view/main/series/SeriesFragment.kt
+++ app/src/main/java/com/affan/movieapp/view/main/series/SeriesFragment.kt
@@ -2,28 +2,27 @@ package com.affan.movieapp.view.main.series
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.affan.movieapp.databinding.FragmentSeriesBinding
 import com.affan.movieapp.model.series.Series
+import com.affan.movieapp.model.series.SeriesResponseViewModel
 import com.affan.movieapp.view.main.details.DetailsActivity
 import com.affan.movieapp.view.main.home.HomeFragment
 import com.affan.movieapp.view.main.series.adapter.SeriesAdapter
-import com.affan.movieapp.view.main.series.presenter.SeriesPresenter
 import com.affan.movieapp.view.main.series.presenter.SeriesView
 
 class SeriesFragment : Fragment(), SeriesView {
 
     private lateinit var binding: FragmentSeriesBinding
     private lateinit var seriesAdapter: SeriesAdapter
-    private lateinit var seriesPresenter: SeriesPresenter
 
+    private val seriesResponseViewModel: SeriesResponseViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,6 +38,14 @@ class SeriesFragment : Fragment(), SeriesView {
 //        createPresenter()
         setSeriesAdapter()
 //        seriesPresenter.getPopularSeries()
+        seriesResponseViewModel.series.observe(requireActivity()){
+            it.series?.let { data->
+                seriesAdapter.setData(data)
+            }
+        }
+        seriesResponseViewModel.errorMessage.observe(requireActivity()){
+            Toast.makeText(requireActivity(), it, Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun setSeriesAdapter() {
@@ -69,10 +76,6 @@ class SeriesFragment : Fragment(), SeriesView {
         intent.putExtra(HomeFragment.EXTRA_DATA_MS, parcelable)
         intent.putExtra(HomeFragment.CATEGORY,"series")
         startActivity(intent)
-    }
-
-    private fun createPresenter() {
-        seriesPresenter = SeriesPresenterImpl(this, lifecycleScope)
     }
 
     override fun onReceiveSeries(series: List<SeriesData>) {
