84bc2be43ea033837610da9ffd16846f1893dedd
Krisna Sirait <krisnapande00@outlook.com>
2022-09-24 20:31:17 +0700

    added SeriesPagingSource.kt

--- app/src/main/java/com/affan/movieapp/model/series/SeriesResponse.kt
+++ app/src/main/java/com/affan/movieapp/model/series/SeriesResponse.kt
@@ -3,11 +3,12 @@ package com.affan.movieapp.model.series
 
 import com.google.gson.annotations.SerializedName
 
+
 data class SeriesResponse(
     @SerializedName("page")
     val page: Int?,
     @SerializedName("results")
-    val series: List<Series?>?,
+    val series: List<Series>,
     @SerializedName("total_pages")
     val totalPages: Int?,
     @SerializedName("total_results")
