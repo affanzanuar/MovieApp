334c69ccf2588cfd89d844127395988e5d39b3c6
Krisna Sirait <krisnapande00@outlook.com>
2022-09-24 20:31:17 +0700

    added SeriesPagingSource.kt

--- app/src/main/java/com/affan/movieapp/network/ApiService.kt
+++ app/src/main/java/com/affan/movieapp/network/ApiService.kt
@@ -2,14 +2,15 @@ package com.affan.movieapp.network
 
 import com.affan.movieapp.model.comingsoon.ComingSoonResponse
 import com.affan.movieapp.model.details.movies.DetailsMovieResponse
-import com.affan.movieapp.model.details.tv.DetailsTvResponse
 import com.affan.movieapp.model.movie.MovieResponse
 import com.affan.movieapp.model.series.SeriesResponse
 import com.affan.movieapp.model.trending.TrendingResponse
+import com.google.gson.annotations.JsonAdapter
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+
 
 interface ApiService {
 
@@ -42,6 +43,12 @@ interface ApiService {
     suspend fun getMostPopularSeries2(
         @Query("api_key") apiKey : String
     ) : retrofit2.Response<SeriesResponse>
+
+    @GET("tv/popular")
+    suspend fun getMostPopularSeries3(
+        @Query("api_key") apiKey : String,
+        @Query("page") page: Int
+    ) : SeriesResponse
 
     @GET("discover/movie")
     fun getComingSoon(
