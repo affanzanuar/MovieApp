c1d427043b6afdc167fdf793fdf6e57c5120f54f
Krisna Sirait <krisnapande00@outlook.com>
2022-09-25 19:03:15 +0700

    optimize import

--- app/src/main/java/com/affan/movieapp/main/series/adapter/PaginationRecyclerView.kt
+++ app/src/main/java/com/affan/movieapp/main/series/adapter/PaginationRecyclerView.kt
@@ -1,7 +1,6 @@
 package com.affan.movieapp.main.series.adapter
 
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 
