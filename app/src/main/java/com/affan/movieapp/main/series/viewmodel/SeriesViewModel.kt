526c268949530ef0b4b46516e53f2e73fdeccd68
Krisna Sirait <krisnapande00@outlook.com>
2022-09-25 21:36:20 +0700

    implement repository in SeriesViewModel.kt

--- app/src/main/java/com/affan/movieapp/main/series/viewmodel/SeriesViewModel.kt
+++ app/src/main/java/com/affan/movieapp/main/series/viewmodel/SeriesViewModel.kt
@@ -4,9 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.affan.movieapp.data.Data
 import com.affan.movieapp.domain.Repository
 import com.affan.movieapp.model.series.SeriesResponse
-import com.affan.movieapp.main.series.SeriesRepository
 import kotlinx.coroutines.launch
 
 class SeriesViewModel(private val repository: Repository) : ViewModel() {
@@ -17,7 +17,7 @@ class SeriesViewModel(private val repository: Repository) : ViewModel() {
 
     init {
         viewModelScope.launch {
-            val response = SeriesRepository().getPopularSeries(1)
+            val response = repository.getPopularSeries(1, Data.apiKey)
             if (response.isSuccessful) {
                 _series.postValue(response.body())
             } else {
@@ -27,7 +27,7 @@ class SeriesViewModel(private val repository: Repository) : ViewModel() {
     }
     fun getPopularSeries(page: Int){
         viewModelScope.launch {
-            val response = SeriesRepository().getPopularSeries(page)
+            val response = repository.getPopularSeries(page, Data.apiKey)
             if (response.isSuccessful) {
                 _series.postValue(response.body())
             }else{
