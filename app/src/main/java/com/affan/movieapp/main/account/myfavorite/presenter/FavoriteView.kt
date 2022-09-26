0ebc38e6ba249ae320516dc4e29b0d887e8033b2
Affan Zanuar <affanzanuar1@gmail.com>
2022-09-26 23:51:10 +0700

    Make FavoritePresenter.kt Deprecated

--- app/src/main/java/com/affan/movieapp/main/account/myfavorite/presenter/FavoriteView.kt
+++ app/src/main/java/com/affan/movieapp/main/account/myfavorite/presenter/FavoriteView.kt
@@ -2,6 +2,7 @@ package com.affan.movieapp.main.account.myfavorite.presenter
 
 import com.affan.movieapp.model.MoviesOrSeries
 
+@Deprecated("Not Used Anymore")
 interface FavoriteView {
     fun onGetDataFavoriteSuccess(data : List<MoviesOrSeries>)
     fun onGetDataFavoriteFailure(message : String)
