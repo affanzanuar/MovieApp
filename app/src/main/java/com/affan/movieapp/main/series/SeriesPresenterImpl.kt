e69f35a8fe4474ce5a6358a1d1c07517edf17496
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-04 21:11:06 +0700

    Remove Call in fun ApiService and remove enqueue in viewmodel home. Add simple function with runcatching

--- app/src/main/java/com/affan/movieapp/main/series/SeriesPresenterImpl.kt
+++ app/src/main/java/com/affan/movieapp/main/series/SeriesPresenterImpl.kt
@@ -20,41 +20,41 @@ class SeriesPresenterImpl(
     private val coroutineScope: CoroutineScope
 ) : SeriesPresenter {
     override fun getPopularSeries() {
-        coroutineScope.launch {
-            withContext(Dispatchers.IO) {
-                ApiClient.instance.getMostPopularSeries(Data.apiKey)
-                    .enqueue(object : Callback<SeriesResponse> {
-                        override fun onResponse(
-                            call: Call<SeriesResponse>,
-                            response: Response<SeriesResponse>
-                        ) {
-                            val body = response.body()!!
-                            coroutineScope.launch {
-                                withContext(Dispatchers.Main) {
-                                    body.series
-                                        .let {
-                                            if (it != null) {
-                                                seriesView.onSuccessGetPopularSeries(it)
-                                                Log.d(
-                                                    "Main Presenter adalah",
-                                                    response.body()?.series.toString()
-                                                )
-                                            }
-                                        }
-                                }
-                            }
-                        }
-
-                        override fun onFailure(call: Call<SeriesResponse>, t: Throwable) {
-                            coroutineScope.launch {
-                                withContext(Dispatchers.Main) {
-                                    seriesView.onFailGetPopularSeries(t.message!!)
-                                }
-                            }
-                        }
-                    })
-            }
-        }
+//        coroutineScope.launch {
+//            withContext(Dispatchers.IO) {
+//                ApiClient.instance.getMostPopularSeries(Data.apiKey)
+//                    .enqueue(object : Callback<SeriesResponse> {
+//                        override fun onResponse(
+//                            call: Call<SeriesResponse>,
+//                            response: Response<SeriesResponse>
+//                        ) {
+//                            val body = response.body()!!
+//                            coroutineScope.launch {
+//                                withContext(Dispatchers.Main) {
+//                                    body.series
+//                                        .let {
+//                                            if (it != null) {
+//                                                seriesView.onSuccessGetPopularSeries(it)
+//                                                Log.d(
+//                                                    "Main Presenter adalah",
+//                                                    response.body()?.series.toString()
+//                                                )
+//                                            }
+//                                        }
+//                                }
+//                            }
+//                        }
+//
+//                        override fun onFailure(call: Call<SeriesResponse>, t: Throwable) {
+//                            coroutineScope.launch {
+//                                withContext(Dispatchers.Main) {
+//                                    seriesView.onFailGetPopularSeries(t.message!!)
+//                                }
+//                            }
+//                        }
+//                    })
+//            }
+//        }
     }
 
 }
