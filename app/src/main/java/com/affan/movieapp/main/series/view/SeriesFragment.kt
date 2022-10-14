44ff583527985a33505bac42d9417034d0c1b78f
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-14 23:31:11 +0700

    Delete unused code Paginate in SeriesFragment.kt

--- app/src/main/java/com/affan/movieapp/main/series/view/SeriesFragment.kt
+++ app/src/main/java/com/affan/movieapp/main/series/view/SeriesFragment.kt
@@ -2,17 +2,13 @@ package com.affan.movieapp.main.series.view
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AbsListView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.affan.movieapp.databinding.FragmentSeriesBinding
 import com.affan.movieapp.di.ViewModelFactory
 import com.affan.movieapp.main.details.DetailsActivity
@@ -35,12 +31,6 @@ class SeriesFragment : Fragment() {
             ViewModelFactory.getInstance(requireContext())
         }
     )
-
-    private var isLoading = false
-
-    private var isLastPage = false
-
-    private var isScrolling = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -68,49 +58,9 @@ class SeriesFragment : Fragment() {
         }
         binding.rvSeries.adapter = seriesAdapter
         binding.rvSeries.layoutManager = mLayoutManager
-//        binding.rvSeries.addOnScrollListener(this.scrollListener)
 
-        binding.rvSeries.addOnScrollListener(object : PaginationRecyclerView({ viewModel.getPopularSeries() }) {
-
-//            override fun loadMoreItems() {
-////                page++
-////                Log.d("checkPageMoreItems", "$page ")
-////                isLoadDataOnProgress = true
-////                viewModel.getPopularSeries(page)
-//                viewModel.getPopularSeries()
-//            }
-
-
-
-        })
-    }
-
-    private var scrollListener = object : RecyclerView.OnScrollListener(){
-        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-            super.onScrolled(recyclerView, dx, dy)
-            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
-            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
-            val mChildCount = layoutManager.childCount
-            val mItemCount = layoutManager.itemCount
-            val isNotLoadingAndNotLastPage = !isLoading && !isLastPage
-            val isLastItem = firstVisibleItemPosition + mChildCount >= mItemCount * 0.5
-            val isNotBeginning = firstVisibleItemPosition >= 0
-            val isTotalMoreThanVisible = mItemCount >= 10
-            val shouldPaginate = isNotLoadingAndNotLastPage && isLastItem &&
-                    isNotBeginning && isTotalMoreThanVisible && isScrolling
-            if (shouldPaginate){
-                viewModel.getPopularSeries()
-                isScrolling = false
-            }
-            Log.e("why SeriesFragment", mItemCount.toString())
-        }
-
-        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-            super.onScrollStateChanged(recyclerView, newState)
-            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
-                isScrolling = true
-            }
-        }
+        binding.rvSeries.addOnScrollListener(
+            object : PaginationRecyclerView({ viewModel.getPopularSeries() }) {})
     }
 
     private fun getObserve(){
