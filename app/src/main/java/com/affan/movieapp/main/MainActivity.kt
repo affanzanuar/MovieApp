c5305e90e3569c32c4eaf7d2989968214740f7ed
Affan Zanuar <affanzanuar1@gmail.com>
2022-10-14 15:52:01 +0700

    Delete unused code in MainActivity.kt

--- app/src/main/java/com/affan/movieapp/main/MainActivity.kt
+++ app/src/main/java/com/affan/movieapp/main/MainActivity.kt
@@ -23,18 +23,6 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-//        val appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.navigation_home,
-//                R.id.navigation_series,
-//                R.id.navigation_movies,
-//                R.id.navigation_account
-//            )
-//        )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-
         navView.setupWithNavController(navController)
     }
 
