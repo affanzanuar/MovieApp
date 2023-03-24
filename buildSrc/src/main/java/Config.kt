512ab3354f42d80db8686d1badbad65f102816b9
Affan Zanuar <affanzanuar1@gmail.com>
2023-03-25 00:04:48 +0700

    Changes all gradle to use Config

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -1,2 +1,12 @@
+import org.gradle.api.JavaVersion
+
 object Config {
+    const val MIN_SDK_VERSION = 21
+    const val TARGET_SDK_VERSION = 33
+    const val COMPILE_SDK_VERSION = 33
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+    const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
+    const val MINIFY_ENABLE_RELEASE = true
+    val COMPILE_OPTIONS_COMPATIBILITY = JavaVersion.VERSION_1_8
 }
--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -1,2 +1,9 @@
 object Modules {
+    const val APP = ":app"
+
+    // Core
+    const val CORE_COMMON = ":core:core_common"
+
+    // Feature
+    const val FEATURE_SEARCH = ":feature:search"
 }
